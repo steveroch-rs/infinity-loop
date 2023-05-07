ad7dbfdec3be9ae000c2a2ad8def166236e16efe
Steven Roch <steveroch-rs@users.noreply.github.com>
2023-05-07 22:56:08 +0200

    Prepare effects for strip

--- include/effects.h
+++ include/effects.h
@@ -5,43 +5,45 @@
 #ifndef INFINITY_LOOP_EFFECTS_H
 #define INFINITY_LOOP_EFFECTS_H
 
+#include <FastLED.h>
+
 // show a certain color on all LEDs
-void showColor(CHSV color);
+void showColor(CRGB *strip, CRGB color);
 
 // blink a certain color
-void blinkColor(CHSV color, float freq);
+void blinkColor(CRGB *strip, CRGB color, float freq);
 
 // breathe a certain color in and out
-void breatheColor(CHSV color, float time);
+void breatheColor(CRGB *strip, CRGB color, float time);
 
 // show a random color on all LEDs
-void showRandom();
+void showRandom(CRGB *strip);
 
 // blink a random color
-void blinkRandom(float freq);
+void blinkRandom(CRGB *strip, float freq);
 
 // brathe a random color in and out
-void breatheRandom(float time);
+void breatheRandom(CRGB *strip, float time);
 
 // fade the entire loop through RGB
-void fadeRGB(float time);
+void fadeRGB(CRGB *strip, float time);
 
 // let a certain color run through the loop
-void colorSpiral(float time);
+void colorSpiral(CRGB *strip, float time);
 
 // let the RGB color-space run through the loop
-void rgbSpiral(float time);
+void rgbSpiral(CRGB *strip, float time);
 
 // show the entire loop as white
-void white();
+void white(CRGB *strip);
 
 // show a spiral of white light on the loop
-void whiteSpiral(float time);
+void whiteSpiral(CRGB *strip, float time);
 
 // show a fire animation
-void fire();
+void fire(CRGB *strip);
 
 // show a christmassy animation with red, green and white color
-void christmas();
+void christmas(CRGB *strip);
 
 #endif //INFINITY_LOOP_EFFECTS_H
--- src/effects.cpp
+++ src/effects.cpp
@@ -1,3 +1,58 @@
 //
 // Created by steve on 5/7/23.
 //
+
+#include "effects.h"
+
+
+void showColor(CRGB *strip, CRGB color) {
+
+}
+
+void blinkColor(CRGB *strip, CRGB color, float freq) {
+
+}
+
+void breatheColor(CRGB *strip, CRGB color, float time) {
+
+}
+
+void showRandom(CRGB *strip) {
+
+}
+
+void blinkRandom(CRGB *strip, float freq) {
+
+}
+
+void breatheRandom(CRGB *strip, float time) {
+
+}
+
+void fadeRGB(CRGB *strip, float time) {
+
+}
+
+void colorSpiral(CRGB *strip, float time) {
+
+}
+
+void rgbSpiral(CRGB *strip, float time) {
+
+}
+
+void white(CRGB *strip) {
+
+}
+
+void whiteSpiral(CRGB *strip, float time) {
+
+}
+
+void fire(CRGB *strip) {
+
+}
+
+void christmas(CRGB *strip) {
+
+}
